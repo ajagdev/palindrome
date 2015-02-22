69cb384788d5294f19f4a1c1c7bb6c039435d664
Abhi Jagdev <abhijagdev@gmail.com>
2015-02-22 02:20:53 -0800

    update

---
+++ palindrome.py
@@ -0,0 +1,23 @@
+import re
+
+def strip(str):
+	pattern = '[a-zA-z]'
+	return ''.join(re.findall(pattern,str))
+
+def is_palindrome(str):
+
+	org = strip(str)
+	rev = strip(str[::-1])
+
+	if org.lower() == rev.lower():
+		print  '"%s" is a palindrome sentence' % str.replace('\n', '')
+	else:
+		print  '"%s" is not palindrome sentence' % str.replace('\n', '')
+
+if __name__ == '__main__':
+	print "---Starting Program---\n"
+
+	for line in open("input.txt" , "r").readlines():
+		is_palindrome(line)
+
+	print "\n---finished running---"	
